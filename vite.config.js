29acd78c888f803cb977bad8ea474e8f09fffb31
Marcos Aurelio <marcos.aurelio@bis2bis.com.br>
2024-03-12 17:47:55 -0300

    ✨ Vite + vue

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, './src/view/adminhtml/web/js/main.js'),
+      output: {
+        dir: resolve(__dirname, './src/view/adminhtml/web/js/dist'),
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js'
+      }
+    }
+  }
+})
